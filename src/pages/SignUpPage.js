f3eed2ad9b02b8273b9dfe4661ce7fc40ce5a506
Nischay23 <khobragadenischay23@gmail.com>
2024-12-28 12:50:01 +0530

    Added Routing , Login and SignUp is created.

---
+++ src/pages/SignUpPage.js
@@ -0,0 +1,11 @@
+import SignUp from "../features/auth/counter/components/SignUp";
+
+function SignupPage() {
+  return (
+    <div>
+      <SignUp></SignUp>
+    </div>
+  );
+}
+
+export default SignupPage;
