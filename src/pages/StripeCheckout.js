7672e5384f11eda3eaa61637a9f608bbfe29cecc
Nischay23 <khobragadenischay23@gmail.com>
2025-01-18 18:40:07 +0530

    Stripe Integration

---
+++ src/pages/StripeCheckout.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import CheckoutForm from "./CheckoutForm";
+import CompletePage from "./CompletePage";
+import "../Stripe.css";
+import { useSelector } from "react-redux";
+import { selectCurrentOrder } from "../features/order/orderSlice";
+
+// Make sure to call loadStripe outside of a component’s render to avoid
+// recreating the Stripe object on every render.
+// This is your test publishable API key.
+const stripePromise = loadStripe(
+  "pk_test_51QiCE6EPFXR1RnvsBmrg1vT8olfTiyCB59x2iMHrBFw0GUTtl9IlqzxOxwY2JHktdQJvDrPk5DbPqGoJwSArMwzZ00VoZ3UW1A"
+);
+
+export default function App() {
+  const [clientSecret, setClientSecret] = useState("");
+  const currentOrder = useSelector(selectCurrentOrder);
+
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch("http://localhost:8080/create-payment-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ totalAmount: currentOrder.totalAmount }),
+      meta: {
+        order_id: currentOrder.id,
+        // this info will go to stripe => and then to our webhook
+        // so we can conclude that payment was successful, even if client closes window after pay
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, []);
+
+  const appearance = {
+    theme: "stripe",
+  };
+  // Enable the skeleton loader UI for optimal loading.
+  const loader = "auto";
+
+  return (
+    <div className="Stripe">
+      {clientSecret && (
+        <Elements
+          options={{ clientSecret, appearance, loader }}
+          stripe={stripePromise}
+        >
+          <CheckoutForm />
+          <CompletePage />
+        </Elements>
+      )}
+    </div>
+  );
+}
